5eebe77c330d6064ddefa94c7b6df709885a69b9
Shadycore <56372248+Shadycore@users.noreply.github.com>
2022-10-13 16:36:53 -0500

    Update models.py

--- syshacienda/mnt/models.py
+++ syshacienda/mnt/models.py
@@ -5,188 +5,32 @@ from baseapp.models import ContactFields
 from baseapp.models import PersonFields
 
 
-class Actividad(BaseFields):
-    idactividad = models.AutoField(primary_key=True)
-    idcultivo = models.IntegerField()
-    nombre = models.CharField(max_length=50)
-    fecha = models.DateField()
-
-    def __str__(self):
-        return self.nombre
-
-    def save (self):
-        pass
-
-    class Meta:
-        verbose_name_plural = "Actividades"
-        db_table = 'actividad'
-
-
-class Asignacion(BaseFields):
-    idasignacion = models.AutoField(primary_key=True)
-    idactividad = models.IntegerField()
-    idempleado = models.IntegerField()
-    idcultivo = models.IntegerField()
-    descripcion = models.CharField(max_length=50)
-    fecha = models.DateField()
-
-    def __str__(self):
-        return self.nombre
-
-    def save (self):
-        pass
-    class Meta:
-        verbose_name_plural = "Asignaciones"
-        db_table = 'asignacion'
-
-
-class AsignacionMaterial(BaseFields):
-    idmaterial = models.AutoField(primary_key=True)
-    idinsumo = models.IntegerField()
-    idcultivo = models.IntegerField()
-    encargado = models.CharField(max_length=50)
-    fecha = models.DateField()
-    
-    def __str__(self):
-        return self.fecha
-
-    def save (self):
-        pass
-    class Meta:
-        verbose_name_plural = "AsignacionMateriales"
-        db_table = 'asignacion_material'
-
-
-class Cliente(PersonFields, ContactFields, BaseFields):
-    idcliente = models.AutoField(primary_key=True)
-    
-    def __str__(self):
-        return self.nombres
-
-    def save (self):
-        pass
-    class Meta:
-        verbose_name_plural = "Clientes"
-        db_table = 'cliente'
-
-class Cosecha(BaseFields):
-    idcosecha = models.AutoField(primary_key=True)
-    idhacienda = models.IntegerField()
-    idcultivo = models.IntegerField()
-    fecha = models.DateField()
-    cantidad = models.FloatField(blank=True, null=True)
-
-    def __str__(self):
-        return self.fecha
-
-    def save (self):
-        pass
-    class Meta:
-        verbose_name_plural = "Cosechas"
-        db_table = 'cosecha'
-
-
+## tablas codigos principales ******************************************** ##
+# - Cultivo - #
 class Cultivo(BaseFields):
-    idcultivo = models.AutoField(primary_key=True)
+    idCultivo = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     lote = models.CharField(max_length=50)
-    fechainicio = models.DateField()
-    fechafin = models.DateField(blank=True, null=True)
+    fechaInicio = models.DateField(blank=True, null=True)
+    fechaFin = models.DateField(blank=True, null=True)
 
     def __str__(self):
-        return self.nombre
+        return '{}'.format(self.nombre)
 
-    def save (self):
-        pass
+    def save(self):
+        self.nombre = self.nombre.capitalize()
+        self.lote = self.lote
+        self.fechaInicio = self.fechaInicio
+        self.fechaFin = self.fechaFin
+        super(Cultivo,self).save()
+
     class Meta:
         verbose_name_plural = "Cultivos"
         db_table = 'cultivo'
 
-class RegistroEmpleado(BaseFields):
-    idregistro = models.AutoField(primary_key=True)
-    idhacienda = models.IntegerField()
-    idempleado = models.IntegerField()
-    contrato = models.CharField(max_length=100)
-    cargo = models.CharField(max_length=30, blank=True, null=True)
-
-    def __str__(self):
-        return self.cargo
-
-    def save (self):
-        pass
-
-    class Meta:
-        verbose_name_plural = "RegistroEmpleados"
-        db_table = 'registro_empleado'
-
-
-class DescripcionLote(BaseFields):
-    iddescripcion_lote = models.AutoField(primary_key=True)
-    idcultivo = models.IntegerField()
-    area = models.FloatField(blank=True, null=True)
-    idproduccion = models.IntegerField()
-    etapa = models.CharField(max_length=50, blank=True, null=True)
-
-    def __str__(self):
-        return self.etapa
-
-    def save (self):
-        pass
-
-    class Meta:
-        verbose_name_plural = "DescripcionLotes"
-        db_table = 'descripcion_lote'
-
-
-class DetalleVenta(BaseFields):
-    iddetalle_venta = models.AutoField(primary_key=True)
-    idventa = models.IntegerField()
-    fecha_venta = models.DateField(blank=True, null=True)
-    idcliente = models.IntegerField()
-    cantidadventa = models.FloatField(blank=True, null=True)
-
-    def __str__(self):
-        return self.cantidadventa
-
-    def save (self):
-        pass 
-
-    class Meta:
-        verbose_name_plural = "DetalleVentas"
-        db_table = 'detalle_venta'
-
-class Empleado(BaseFields, PersonFields, ContactFields):
-    idempleado = models.AutoField(primary_key=True)
-    fchnacimiento = models.DateField()
-
-    def __str__(self):
-        return self.nombres
-
-    def save (self):
-        pass
-       
-    class Meta:
-        verbose_name_plural = "Empleados"
-        db_table = 'empleado'
-
-
-class Hacienda(BaseFields, PersonFields, ContactFields ):
-    idhacienda = models.AutoField(primary_key=True)
-
-    def __str__(self):
-        return self.nombre
-
-    def save (self):
-        pass
-       
-
-    class Meta:
-        verbose_name_plural = "Haciendas"
-        db_table = 'hacienda'
-
-
+# - Insumo - #
 class Insumo(BaseFields):
-    idinsumo = models.AutoField(primary_key=True)
+    idInsumo = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     tipo = models.CharField(max_length=50)
     uso = models.CharField(max_length=50)
@@ -202,28 +46,38 @@ class Insumo(BaseFields):
         verbose_name_plural = "Insumos"
         db_table = 'insumo'
 
-
-class Produccion(BaseFields):
-    idproduccion = models.AutoField(primary_key=True)
-    idcultivo = models.IntegerField()
-    idinsumo = models.IntegerField()
-    fecha = models.DateField()
-    cantcosecha = models.IntegerField(blank=True, null=True)
+# - Hacienda - #
+class Hacienda(BaseFields, PersonFields, ContactFields ):
+    idhacienda = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return self.fecha
+        return self.nombre
+
+    def save (self):
+        pass
+
+    class Meta:
+        verbose_name_plural = "Haciendas"
+        db_table = 'hacienda'
+
+## tablas entes ******************************************** ##
+# - Empleado - #
+class Empleado(BaseFields, PersonFields, ContactFields):
+    idEmpleado = models.AutoField(primary_key=True)
+    fchNacimiento = models.DateField()
+
+    def __str__(self):
+        return self.nombres
 
     def save (self):
         pass
        
-
     class Meta:
-        verbose_name_plural = "Producciones"
-        db_table = 'produccion'
-
-
+        verbose_name_plural = "Empleados"
+        db_table = 'empleado'
+# - Proveedor - #
 class Proveedor(BaseFields, PersonFields, ContactFields):
-    idproveedor = models.AutoField(primary_key=True)
+    idProveedor = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
@@ -236,16 +90,154 @@ class Proveedor(BaseFields, PersonFields, ContactFields):
         verbose_name_plural = "Proveedores"
         db_table = 'proveedor'
 
+# - Cliente - #
+class Cliente(PersonFields, ContactFields, BaseFields):
+    idCliente = models.AutoField(primary_key=True)
+    
+    def __str__(self):
+        return self.nombres
 
+    def save (self):
+        pass
+    class Meta:
+        verbose_name_plural = "Clientes"
+        db_table = 'cliente'
+
+
+## tablas codigos relacionados ******************************************** ##
+# - Actividad - #
+class Actividad(BaseFields):
+    idActividad = models.AutoField(primary_key=True)
+    idCultivo = models.IntegerField()
+    nombre = models.CharField(max_length=50)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+    def save (self):
+        pass
+
+    class Meta:
+        verbose_name_plural = "Actividades"
+        db_table = 'actividad'
+
+# - Asignacion - #
+class Asignacion(BaseFields):
+    idAsignacion = models.AutoField(primary_key=True)
+    idActividad = models.IntegerField()
+    idEmpleado = models.IntegerField()
+    idCultivo = models.IntegerField()
+    descripcion = models.CharField(max_length=50)
+    fecha = models.DateField()
+
+    def __str__(self):
+        return self.nombre
+
+    def save (self):
+        pass
+    class Meta:
+        verbose_name_plural = "Asignaciones"
+        db_table = 'asignacion'
+
+# - AsignacionMaterial - #
+class AsignacionMaterial(BaseFields):
+    idMaterial = models.AutoField(primary_key=True)
+    idInsumo = models.IntegerField()
+    idCultivo = models.IntegerField()
+    encargado = models.CharField(max_length=50)
+    fecha = models.DateField()
+    
+    def __str__(self):
+        return self.fecha
+
+    def save (self):
+        pass
+    class Meta:
+        verbose_name_plural = "AsignacionMateriales"
+        db_table = 'asignacion_material'
+
+# - Cosecha - #
+class Cosecha(BaseFields):
+    idCosecha = models.AutoField(primary_key=True)
+    idHacienda = models.IntegerField()
+    idCultivo = models.IntegerField()
+    fecha = models.DateField()
+    cantidad = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fecha
+
+    def save (self):
+        pass
+    class Meta:
+        verbose_name_plural = "Cosechas"
+        db_table = 'cosecha'
+
+# - Registro Empleado - #
+class RegistroEmpleado(BaseFields):
+    idRegistro = models.AutoField(primary_key=True)
+    idHacienda = models.IntegerField()
+    idEmpleado = models.IntegerField()
+    contrato = models.CharField(max_length=100)
+    cargo = models.CharField(max_length=30, blank=True, null=True)
+
+    def __str__(self):
+        return self.cargo
+
+    def save (self):
+        pass
+
+    class Meta:
+        verbose_name_plural = "RegistroEmpleados"
+        db_table = 'registro_empleado'
+
+# - DescripcionLote - #
+class DescripcionLote(BaseFields):
+    idDescripcionLote = models.AutoField(primary_key=True)
+    idCultivo = models.IntegerField()
+    area = models.FloatField(blank=True, null=True)
+    idProduccion = models.IntegerField()
+    etapa = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.etapa
+
+    def save (self):
+        pass
+
+    class Meta:
+        verbose_name_plural = "DescripcionLotes"
+        db_table = 'descripcion_lote'
+
+# - Produccion - #
+class Produccion(BaseFields):
+    idProduccion = models.AutoField(primary_key=True)
+    idCultivo = models.IntegerField()
+    idInsumo = models.IntegerField()
+    fecha = models.DateField()
+    cantidadCosecha = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.fecha
+
+    def save (self):
+        pass
+
+    class Meta:
+        verbose_name_plural = "Producciones"
+        db_table = 'produccion'
+
+# - RegistroInsumo - #
 class RegistroInusmo(BaseFields):
-    idregistro_insumo = models.AutoField(primary_key=True)
-    fechacompra = models.DateField()
+    idRegistroInsumo = models.AutoField(primary_key=True)
+    idInsumo = models.IntegerField()
+    idCultivo = models.IntegerField()
+    fechaCompra = models.DateField()
     precio = models.TextField() 
-    fechaingreso = models.DateField()
-    fechaexpira = models.DateField()
+    fechaIngreso = models.DateField()
+    fechaExpira = models.DateField()
     requerimiento = models.CharField(max_length=100, blank=True, null=True)
-    idinsumo = models.IntegerField()
-    idcultivo = models.IntegerField()
 
     def __str__(self):
         return self.fechaingreso
@@ -257,10 +249,12 @@ class RegistroInusmo(BaseFields):
         verbose_name_plural = "RegistroInsumos"
         db_table = 'registro_inusmo'
 
+## Venta ******************************************** ##
+# - Venta - #
 class Venta(BaseFields):
-    idventa = models.AutoField(primary_key=True)
-    idcultivo = models.IntegerField()
-    idproduccion = models.IntegerField()
+    idVenta = models.AutoField(primary_key=True)
+    idCultivo = models.IntegerField()
+    idProduccion = models.IntegerField()
     cantidad = models.FloatField()
     precio = models.TextField()  
     fecha = models.DateField()
@@ -275,4 +269,22 @@ class Venta(BaseFields):
     class Meta:
         verbose_name_plural = "Ventas"
         db_table = 'venta'
+
+# - DetalleVenta - #
+class DetalleVenta(BaseFields):
+    idDetalleVenta = models.AutoField(primary_key=True)
+    idVenta = models.IntegerField()
+    idCliente = models.IntegerField()
+    fechaVenta = models.DateField(blank=True, null=True)
+    cantidadVenta = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return self.cantidadVenta
+
+    def save (self):
+        pass 
+
+    class Meta:
+        verbose_name_plural = "DetalleVentas"
+        db_table = 'detalle_venta'
 
