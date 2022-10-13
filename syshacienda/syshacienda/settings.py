4e04142d7abb77d47b93e7891e6b359d38b1b319
Shadycore <walter.lopez.calle@hotmail.com>
2022-10-13 00:53:04 -0500

    v0.1.0

--- syshacienda/syshacienda/settings.py
+++ syshacienda/syshacienda/settings.py
@@ -132,9 +132,10 @@ USE_TZ = True
 MEDIA_ROOT = (os.path.join(BASE_DIR,'media'),)
 MEDIA_URL = '/media/'
 
+STATIC_URL = '/static/'
 STATICFILES_DIR = (os.path.join(BASE_DIR,'static'),)
-STATIC_ROOT = (os.path.join(BASE_DIR,'static'),)
-STATIC_URL = 'static/'
+#STATIC_ROOT = (os.path.join(BASE_DIR,'static'),)
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
