5330d9573a37ca3a5f09a959a110e8a142d0ea98
Shadycore <56372248+Shadycore@users.noreply.github.com>
2022-10-17 17:10:46 -0500

    Update views.py

--- syshacienda/mnt/views.py
+++ syshacienda/mnt/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from mnt.models import Cultivo
 
 class CultivoList(LoginRequiredMixin, generic.ListView):
-    pass
+    model = Cultivo
+    template_name="mnt/Cultivolist.html"
+    context_object_name = "obj"
+    login_url = "baseapp:login"
