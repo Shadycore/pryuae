bba0b3fb79554518f8cf4afb8e2562a0a54828c7
Shadycore <56372248+Shadycore@users.noreply.github.com>
2022-12-23 18:12:32 +0000

    Venta 0.6.1

--- syshacienda/api/urls.py
+++ syshacienda/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from api.views import ProduccionList, ProduccionDetalle, ClienteList
 
 urlpatterns = [
-    path('v1/Produccion/',ProduccionList.as_view(),name='produccion_list'),
-    path('v1/ProduccionDetalle/<str:codigo>',ProduccionDetalle.as_view(),name='produccion_detalle'),
-    path('v1/clientes/',ClienteList.as_view(),name='cliente_list')
+    path('v1/ProduccionList/',ProduccionList.as_view(),name='ProduccionList'),
+    path('v1/ProduccionDetalle/<str:codigo>',ProduccionDetalle.as_view(),name='ProduccionDetalle'),
+    path('v1/clientes/',ClienteList.as_view(),name='ClienteList')
 ]
