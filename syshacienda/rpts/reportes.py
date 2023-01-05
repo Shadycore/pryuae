82dd641266bac336121a305d44e7d62d6ffa6c84
Shadycore <56372248+Shadycore@users.noreply.github.com>
2023-01-05 20:37:01 +0000

    Reportes v0.0.3

---
+++ syshacienda/rpts/reportes.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.utils.dateparse import parse_date
+from datetime import timedelta
+
+from django.db import models
+from vent.models import Venta, DetalleVenta
+from mnt.models import Cliente, Cultivo, Produccion, Empleado, Asignacion, Proveedor
+
+
+def imprimirLista(request,id,f1=None,f2=None):
+    template_name="rpts/imprimirlista.html"
+    #Clientes
+    if id == 1:
+        cliente = Cliente.objects.all().order_by('-id')
+        context={
+            'opt'   : "1",
+            'titulo':"Informe de cliente",
+            'cabecera': {"id", "Apellido", "Nombre", "Email", "Teléfono", "Ciudad" },
+            'detalle': cliente
+        }
+
+    #Proveedores
+    if id ==2:
+        vent = Venta.objects.get(id=id)
+        det = DetalleVenta.objects.filter(venta=id)
+
+        context={
+            'request':request,
+            'venta':vent,
+            'detalle':det
+        }
+    
+    #compras
+    if id == 3:
+        vent = Venta.objects.get(id=id)
+        det = DetalleVenta.objects.filter(venta=id)
+        context={
+            'request':request,
+            'venta':vent,
+            'detalle':det
+        }
+
+    return render(request,template_name,context)
--- syshacienda/rpts/urls.py
+++ syshacienda/rpts/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path, include
 from rpts.views import iProveedoresView, iClientesView, iComprasView 
+from rpts.reportes import imprimirLista
+
 urlpatterns = [
     #Reportes
-    path('Informe/proveedores', iProveedoresView.as_view(template_name='rpts/iProveedores.html'), name='iproveedores'),
-    path('Informe/clientes', iClientesView.as_view(template_name='rpts/iClientes.html'), name='iclientes'),
-   path('Informe/compras', iComprasView.as_view(template_name='rpts/iCompras.html'), name='icompras'),
+    path('informe/proveedores', iProveedoresView.as_view(template_name='rpts/iProveedores.html'), name='iproveedores'),
+    path('informe/clientes', iClientesView.as_view(template_name='rpts/iClientes.html'), name='iclientes'),
+   path('informe/compras', iComprasView.as_view(template_name='rpts/iCompras.html'), name='icompras'),
+
+    #Impresión
+    path('informe/imprimir/<int:id>', imprimirLista, name="imprimelista"),
+    path('informe/imprimircompra/<int:id>/<str:f1>/<str:f2>', imprimirLista, name="imprimecompra"),
 ]
