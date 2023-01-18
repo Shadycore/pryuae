03e3bd7376f209d9f47eafd45e5050908fb797f7
Shadycore <56372248+Shadycore@users.noreply.github.com>
2023-01-18 21:52:54 +0000

    BI v0.0.1

---
+++ syshacienda/bi/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'bi'
---
+++ syshacienda/bi/views.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.shortcuts import render,redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views import generic
+from django.urls import reverse, reverse_lazy
+from django.db.models import Sum, F, DateTimeField, Count, FloatField, IntegerField, Prefetch, Case, When
+from datetime import datetime, timezone
+from django.db.models.functions import TruncMonth, TruncYear, ExtractMinute, ExtractMonth, ExtractYear, Cast, Coalesce
+from django.db.models.query import Q
+import json
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib import messages
+from django.contrib.auth import authenticate
+
+from mnt.views import ClienteView, CultivoView, ProduccionView, \
+                            HaciendaView, EmpleadoNew, AsignacionView, \
+                            ProveedorNew, RegistroInsumoNew
+from mnt.models import Cliente, Cultivo, Produccion, DescripcionLote, \
+                        Hacienda, Empleado, Asignacion, \
+                        Proveedor, RegistroInsumo, Insumo, \
+                        Parametro
+from vent.views import ventasView, VentaView
+from vent.models import Venta, DetalleVenta
+# Create your views here.
+
+@login_required(login_url='/login/')
+def bi(request):
+    template_name = 'bi.html'
+    if request.method == 'POST':
+        pass
+    context = {}
+    return render(request, template_name, context)
