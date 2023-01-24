479d13188b0ff060a7b3b314b5a9b85417c34474
Shadycore <56372248+Shadycore@users.noreply.github.com>
2023-01-24 16:13:31 +0000

    Publicacion 2.1.0

--- syshacienda/baseapp/views.py
+++ syshacienda/baseapp/views.py
@@ -117,18 +117,20 @@ def Home(request):
             ventas_semana.append({'fecha_venta': fecha_venta, 'total_venta': 0})
 
     ventas = Venta.objects.filter(fechaVenta__gte=datetime.now() - timedelta(days=(tiempobi+8))).values('fechaVenta', 'totalVenta')
-
-    #realizamos la proyección basado en el parametro tiempobi: 180 días
-    df = pd.DataFrame(ventas)
-    df['fechaVenta'] = pd.to_datetime(df['fechaVenta']) # Convertimos la columna fechaVenta a tipo datetime
-    df['dias'] = (df['fechaVenta'] - df['fechaVenta'].min())  / np.timedelta64(1,'D') # Creamos una columna con el número de días desde el primer registro de venta 
-    X = df[['dias']] # Definimos X como la columna dias del DataFrame 
-    y = df[['totalVenta']] # Definimos y como la columna totalVenta del DataFrame 
-    model = LinearRegression() # Creamos un modelo de regresión lineal 
-    model.fit(X, y) # Entrenamos el modelo con los datos obtenidos 
-    predicciones = model.predict([[181], [182], [183], [184], [185], [186], [187], [188]]) # Realizamos predicciones para los siguientes 8 días 
-    pred_formateada = np.round(predicciones, decimals=0).tolist() # Redondeamos las predicciones a 0 decimales 
-    pred_formateada =  [int(numero[0]) for numero in pred_formateada] # formateamos la lsta
+    if ventas:
+        #realizamos la proyección basado en el parametro tiempobi: 180 días
+        df = pd.DataFrame(ventas)
+        df['fechaVenta'] = pd.to_datetime(df['fechaVenta']) # Convertimos la columna fechaVenta a tipo datetime
+        df['dias'] = (df['fechaVenta'] - df['fechaVenta'].min())  / np.timedelta64(1,'D') # Creamos una columna con el número de días desde el primer registro de venta 
+        X = df[['dias']] # Definimos X como la columna dias del DataFrame 
+        y = df[['totalVenta']] # Definimos y como la columna totalVenta del DataFrame 
+        model = LinearRegression() # Creamos un modelo de regresión lineal 
+        model.fit(X, y) # Entrenamos el modelo con los datos obtenidos 
+        predicciones = model.predict([[181], [182], [183], [184], [185], [186], [187], [188]]) # Realizamos predicciones para los siguientes 8 días 
+        pred_formateada = np.round(predicciones, decimals=0).tolist() # Redondeamos las predicciones a 0 decimales 
+        pred_formateada =  [int(numero[0]) for numero in pred_formateada] # formateamos la lsta
+    else:
+         pred_formateada =  [0,0,0,0,0,0,0,0] # formateamos la lsta
 
     context = {'anioactual': anioactual, 'anioanterior': anioanterior,
     'tventasanio': tventasanio, 'tventasanioanterior': tventasanioanterior,
