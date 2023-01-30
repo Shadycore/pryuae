495612feb554271794207ba9b682a1925f7a0287
Shadycore <56372248+Shadycore@users.noreply.github.com>
2023-01-30 18:02:13 +0000

    Reportes 2.1.5

--- syshacienda/baseapp/views.py
+++ syshacienda/baseapp/views.py
@@ -117,7 +117,9 @@ def Home(request):
             ventas_semana.append({'fecha_venta': fecha_venta, 'total_venta': 0})
 
     ventas = Venta.objects.filter(fechaVenta__gte=datetime.now() - timedelta(days=(tiempobi+8))).values('fechaVenta', 'totalVenta')
-    if ventas:
+    if not ventas:
+        pred_formateada =  [0,0,0,0,0,0,0,0] # formateamos la lsta
+    else:
         #realizamos la proyección basado en el parametro tiempobi: 180 días
         df = pd.DataFrame(ventas)
         df['fechaVenta'] = pd.to_datetime(df['fechaVenta']) # Convertimos la columna fechaVenta a tipo datetime
@@ -129,8 +131,6 @@ def Home(request):
         predicciones = model.predict([[181], [182], [183], [184], [185], [186], [187], [188]]) # Realizamos predicciones para los siguientes 8 días 
         pred_formateada = np.round(predicciones, decimals=0).tolist() # Redondeamos las predicciones a 0 decimales 
         pred_formateada =  [int(numero[0]) for numero in pred_formateada] # formateamos la lsta
-    else:
-         pred_formateada =  [0,0,0,0,0,0,0,0] # formateamos la lsta
          
 
     context = {'anioactual': anioactual, 'anioanterior': anioanterior,
