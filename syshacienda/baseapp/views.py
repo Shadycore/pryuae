90559a2ee9caafb9aee171a4ed107d3a2b89bd04
Shadycore <walter.lopez.calle@hotmail.com>
2023-03-05 23:52:23 -0500

    Novedades

--- syshacienda/baseapp/views.py
+++ syshacienda/baseapp/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.db.models import Sum, F, DateTimeField, Count, FloatField, IntegerField, Prefetch, Case, When
 from datetime import datetime, timezone, timedelta, date
-from django.db.models.functions import TruncMonth, TruncYear, ExtractMinute, ExtractMonth, ExtractYear, Cast, Coalesce, TruncDay, Date
+from django.db.models.functions import TruncMonth, TruncYear, ExtractMinute, ExtractMonth, ExtractYear, Cast, Coalesce, TruncDay
 from django.utils.dateparse import parse_date
 import json
 from django.http import HttpResponse
